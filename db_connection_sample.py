da4ea6f543c3d7eadcf256166ee39916ee3407ef
Samarendra Hedaoo <samarendra.hedaoo@oregonstate.edu>
2018-11-24 17:33:41 -0800

    A Flask webapp demonstrating end-to-end interaction with database

---
+++ db_connection_sample.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from db_connector import connect_to_database, execute_query
+
+app = Flask(__name__)
+
+#the route is what you will type in browser
+@app.route('/hello')
+#the name of this function is just a cosmetic thing
+def hello():
+    #this is the output returned to browser
+    return "Hello world!"
+
+@app.route('/')
+def index():
+    return "<i>Are you looking for /db-test or /hello ?</i>"
+
+@app.route('/db-test')
+def test_database_connection():
+    print("Executing a sample query on the database using the credentials from db_credentials.py")
+    db = connect_to_database()
+    query = "SELECT * from bsg_people;"
+    rows = execute_query(db, query);
+    return render_template('db_test.html', rows=rows)
