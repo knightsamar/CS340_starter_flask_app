6e6b32a0b49b00610bf5cd99895fcae1e103c52b
Samarendra M Hedaoo <samarendra.hedaoo@oregonstate.edu>
2019-03-02 00:44:23 -0800

    code for showing update form with data

--- starter_website/webapp.py
+++ starter_website/webapp.py
@@ -40,3 +40,28 @@ def add_new_people():
         data = (fname, lname, age, homeworld)
         execute_query(db_connection, query, data)
         return ('Person added!');
+
+#display update form and process any updates, using the same function
+@webapp.route('/update_people/<id>', methods=['POST','GET'])
+def update_people():
+    db_connection = connect_to_database()
+    if request.method == 'GET':
+        planets_query = 'SELECT planet_id, name from bsg_planets'
+        planets_results = execute_query(db_connection, planets_query).fetchall();
+        
+        people_query = 'SELECT fname, lname, homeworld, age from bsg_people
+        WHERE character_id == %s' % (id) 
+        people_result = execute_query(db_connection, people_query).fetchone()
+
+        return render_template('people_update.html', planets = planets_results, people = people_result)
+    elif request.method == 'POST':
+        print("Update people!");
+        fname = request.form['fname']
+        lname = request.form['lname']
+        age = request.form['age']
+        homeworld = request.form['homeworld']
+
+        query = 'INSERT INTO bsg_people (fname, lname, age, homeworld) VALUES (%s,%s,%s,%s)'
+        data = (fname, lname, age, homeworld)
+        execute_query(db_connection, query, data)
+        return ('Person added!');
