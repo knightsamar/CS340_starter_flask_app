246a2b5404d4ebc1360dcbea86dace58c4d1fec1
hiroto <kazamah@oregonstate.edu>
2021-11-30 09:05:13 +0900

    added delete function for librarians.
    update function needs to be fixed.

--- starter_website/webapp.py
+++ starter_website/webapp.py
@@ -33,7 +33,7 @@ def browse_loans():
 def browse_librarians():
     print("Fetching and rendering Librarians web page")
     db_connection = connect_to_database()
-    query = "SELECT librarian_name FROM Librarians;"
+    query = "SELECT librarian_name, librarian_id FROM Librarians;"
     result = execute_query(db_connection, query).fetchall()
     print(result)
     return render_template('librarians_browse.html', rows=result)
@@ -60,16 +60,12 @@ def add_new_book():
 def add_new_librarian():
     db_connection = connect_to_database()
     if request.method == 'GET':
-        query = 'SELECT librarian_id, librarian_name from Librarians'
-        result = execute_query(db_connection, query).fetchall()
-        print(result)
-
-        return render_template('librarians_add_new.html', librarian = result)
+        return render_template('librarians_add_new.html')
     elif request.method == 'POST':
         print("Add new librarian!")
-        Lname = request.form['librarian_name']
+        name = request.form['librarian_name']
         query = 'INSERT INTO Librarians (librarian_name) VALUES (%s)'
-        data = (Lname)
+        data = (name)
         execute_query(db_connection, query, data)
         return redirect('/browse_librarians')
 
@@ -176,6 +172,69 @@ def delete_book(id):
     result = execute_query(db_connection, query, data)
     # return (str(result.rowcount) + "row deleted")
     return redirect('/browse_books')
+
+@webapp.route('/update_librarians/<int:id>', methods=['POST','GET'])
+def update_librarians(id):
+    print('In the function')
+    db_connection = connect_to_database()
+    #display existing data
+    if request.method == 'GET':
+        print('The GET request')
+        librarian_query = 'SELECT librarian_name, librarian_id from Librarians WHERE librarian_id = %s' % (id)
+        librarian_result = execute_query(db_connection, librarian_query).fetchone()
+
+        if librarian_result == None:
+            return "No such librarian found!"
+
+        return render_template('librarians_update.html', librarian = librarian_result)
+    elif request.method == 'POST':
+        print('The POST request')
+        librarian_id = request.form['librarian_id']
+        librarian_name = request.form['librarian_name']
+
+        query = "UPDATE Librarians SET librarian_name = %s WHERE librarian_id = %s"
+        data = ( librarian_name, librarian_id)
+        result = execute_query(db_connection, query, data)
+        print(str(result.rowcount) + " row(s) updated")
+
+        return redirect('/browse_librarians')
+
+@webapp.route('/delete_librarians/<int:id>')
+def delete_librarian(id):
+    '''deletes a person with the given id'''
+    db_connection = connect_to_database()
+    query = "DELETE FROM Librarians WHERE librarian_id = %s"
+    data = (id,)
+
+    result = execute_query(db_connection, query, data)
+    # return (str(result.rowcount) + "row deleted")
+    return redirect('/browse_librarians')
+
+@webapp.route('/update_loans/<int:id>', methods=['POST','GET'])
+def update_loans(id):
+    print('In the function')
+    db_connection = connect_to_database()
+    #display existing data
+    if request.method == 'GET':
+        print('The GET request')
+        loan_query = 'SELECT Loans.loan_date, Books.book_title, Loans.loan_is_active, Patrons.patron_name, Librarians.librarian_name FROM Loans JOIN Books ON Loans.book_id = Books.book_id JOIN Patrons ON Loans.patron_id=Patrons.patron_id JOIN Librarians ON Loans.librarian_id=Librarians.librarian_id WHERE book_id = %s' % (id)
+        loan_result = execute_query(db_connection, loan_query).fetchone()
+
+        if loan_result == None:
+            return "No such book found!"
+
+        return render_template('loans_update.html', loans = loan_result)
+    elif request.method == 'POST':
+        print('The POST request')
+        loan_is_active = request.form['loan_is_active']
+        book_id = request.form['book_id']
+
+        query = "UPDATE Loans SET loan_is_active = %s, WHERE book_id = %s"
+        data = (loan_is_active, book_id)
+        result = execute_query(db_connection, query, data)
+        print(str(result.rowcount) + " row(s) updated")
+
+        return redirect('/browse_loans')
 
 @webapp.route('/')
 def index():
